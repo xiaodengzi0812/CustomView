78fe0edcff673d3f4efac7fa287c4dd736e0b7ee
“用dengjiankai <dengjiankai@wanda.cn>
2017-10-06 23:05:36 +0800

    加载动画3

--- app/src/main/java/com/dengzi/customview/LoadingActivity.java
+++ app/src/main/java/com/dengzi/customview/LoadingActivity.java
@@ -1,6 +1,7 @@
 package com.dengzi.customview;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -12,28 +13,42 @@ import com.dengzi.loading.effect1.LoadingView;
  * @Version:1.0.0
  */
 public class LoadingActivity extends AppCompatActivity {
-    private LoadingView loadingView1;
-
+    private com.dengzi.loading.effect1.LoadingView loadingView1;
     private com.dengzi.loading.effect2.LoadingView loadingView2;
+    private com.dengzi.loading.effect3.LoadingView loadingView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loading_activity);
-        loadingView1 = (LoadingView) findViewById(R.id.loading_view1);
+        loadingView1 = (com.dengzi.loading.effect1.LoadingView) findViewById(R.id.loading_view1);
         loadingView2 = (com.dengzi.loading.effect2.LoadingView) findViewById(R.id.loading_view2);
-        loadingView1.setVisibility(View.GONE);
-        loadingView2.setVisibility(View.GONE);
+        loadingView3 = (com.dengzi.loading.effect3.LoadingView) findViewById(R.id.loading_view3);
     }
 
     public void effect1(View view) {
-        loadingView1.setVisibility(View.VISIBLE);
         loadingView2.setVisibility(View.GONE);
+        loadingView3.setVisibility(View.GONE);
+        loadingView1.setVisibility(View.VISIBLE);
     }
 
     public void effect2(View view) {
         loadingView1.setVisibility(View.GONE);
+        loadingView3.setVisibility(View.GONE);
         loadingView2.setVisibility(View.VISIBLE);
+    }
+
+    public void effect3(View view) {
+        loadingView1.setVisibility(View.GONE);
+        loadingView2.setVisibility(View.GONE);
+        loadingView3.setVisibility(View.VISIBLE);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                loadingView3.disappear();
+            }
+        }, 3000);
     }
 
 }
